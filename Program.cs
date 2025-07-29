09f89e791e198fbe0f6c9c8d45d7fc5394b4f26c
IceYush <regmi.aayushh@gmail.com>
2025-07-28 23:11:35 -0400

    Setting up for angular project connection

--- Program.cs
+++ Program.cs
@@ -9,7 +9,20 @@ builder.Configuration["ConnectionStrings:Redis"] = "localhost:6379";
 builder.Services.AddSingleton<RedisService>();
 
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAngular");
 
 app.MapControllers();
 app.Run();
